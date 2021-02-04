7e8e9c1d876c843c6ad063aee5cb0ff9debfb94d
Dagmara <przygocka.dagmara@gmail.com>
2021-02-04 13:25:00 +0100

    Exercise 1 and 2

--- 1/0._Misc/1._Intro_Exercises/1.Basics.js
+++ 1/0._Misc/1._Intro_Exercises/1.Basics.js
@@ -1,7 +1,7 @@
 // --------------------------------------
 // Variables, strings, numbers, floats
 // --------------------------------------
-// Exercise 1 - Console and constants
+// Exercise 1 - Console and constiables
 
 const firstName = "Anders";
 const lastName = "Latif";
@@ -10,32 +10,19 @@ const lastName = "Latif";
 // show in the console
 // My first name is Anders and my last name is Latif
 
-// concatenation
-console.log("My first name is " + firstName + " and my last name is " + lastName);
-
-// string literal
-console.log(`My first name is ${firstName} and my last name is ${lastName}`);
-
+console.log("My First name is " + firstName + " and my last name is " + lastName);
+ 
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
 
 const year = "2020";
 const number = 1;
 
-// Add the year plus the number
-// The result should be 2021
+// Add the year plus the number 
+console.log(parseInt(year) + number)
+//second solution Number(year)
+// The result should be 2020
 // You cannot touch line 1 or 2
-
-const thisYear = parseInt(year) + number;
-console.log(thisYear);
-
-const currentYear = Number(year) + number;
-console.log(currentYear);
-
-console.log(Number("123abc"));
-console.log(parseInt("123abc456"));
-
-console.log(+ year + number);
 
 
 // --------------------------------------
--- 1/0._Misc/1._Intro_Exercises/2.Strings.js
+++ 1/0._Misc/1._Intro_Exercises/2.Strings.js
@@ -4,10 +4,11 @@
 const numberOne = "1.10";
 const numberTwo = "2.30";
 
+const sum = Number(numberOne) + Number(numberTwo)
+console.log(sum)
 // add those two numbers and show the result
 // you cannot touch line 1 neither line 2
 
-console.log(parseFloat(numberOne) + parseFloat(numberTwo));
 
 // --------------------------------------
 
@@ -17,10 +18,8 @@ console.log(parseFloat(numberOne) + parseFloat(numberTwo));
 
 const anotherNumberOne = "1.10";
 const anotherNumberTwo = "2.30";
-
-const solution = parseFloat(anotherNumberOne) + parseFloat(anotherNumberTwo);
-
-console.log(solution.toFixed(2));
+const sumWithDecimals = Number(anotherNumberOne) + Number(anotherNumberTwo)
+console.log(sumWithDecimals.toFixed(2))
 
 // --------------------------------------
 // Exercise 5 - Decimals and average
@@ -30,11 +29,8 @@ const two = 45;
 const three = 98;
 
 // Show in the console the avg. with 5 decimals
-
-const average = (one + two + three) / 3;
-
-console.log(average.toFixed(5));
-
+const summary = (one + two + three)/3
+console.log(summary.toFixed(5))
 
 
 // --------------------------------------
@@ -42,8 +38,7 @@ console.log(average.toFixed(5));
 
 const letters = "abc";
 // Get me the character "c"
-
-
+console.log(letters[2])
 
 // --------------------------------------
 // Exercise 7 - Replace
@@ -51,8 +46,8 @@ const letters = "abc";
 const fact = "You are learning javascript!";
 
 // capitalize the J in Javascript
-
-
+const replaced = fact.replace("javascript", "Javascript")
+console.log(replaced)
 // --------------------------------------
 
 
