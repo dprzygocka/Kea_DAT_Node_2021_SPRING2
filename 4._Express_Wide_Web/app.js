932863a872ccd4e2bacd6420d064e1f6fa38e9d9
Dagmara <przygocka.dagmara@gmail.com>
2021-04-22 19:42:23 +0200

    node-fetch homework plus additional exercises from classes

--- 4._Express_Wide_Web/app.js
+++ 4._Express_Wide_Web/app.js
@@ -1,13 +1,42 @@
 const express = require("express");
 const app = express();
+const fetch = require("node-fetch");
+
+//homework: Implement node-fetch in a route called proxy that calls google and serves that webpage page. Result: When calling /proxy you should see a Google-like homepage. 
+
+app.get('/proxy', (req,res) => {
+    fetch('https://google.com/')
+    .then(res => res.textConverted())
+    .then(body => res.send(body))
+})
+
+
+
+
+
+
+
+
+
+
+//rest of the lesson 
+
+
+app.use(express.static('public'));
 
 app.get('/', (req,res) => {
 //__dirname return path to server
     res.sendFile(__dirname + '/public/welcome.html')
 })
 
-app.get('/dragons', (req, res) => {
-    res.sendFile(__dirname + '/public/dragons.html')
-})
+app.get('/currency', (req,res) => {
+    //__dirname return path to server
+        res.sendFile(__dirname + '/ajax.html')
+    })
 
-app.listen(8080, e => e ? console.log(e) : 'server is running on: ',8080);
+app.get('/dragons', (req, res) => {
+    res.sendFile(__dirname + '/public/dragons/dragons.html')
+})
+const PORT = process.env.PORT || 8080;
+console.log("Port is running on :", PORT)
+app.listen(PORT, e => e ? console.log(e) : 'server is running on: ',PORT);
